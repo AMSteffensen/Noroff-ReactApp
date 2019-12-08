c7a2ce6455024e5eff0a17185ae0f04d18ff6f2c
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-12-08 09:33:18 +0100

    add search and filter characters

--- src/components/characters/list/CharacterList.js
+++ src/components/characters/list/CharacterList.js
@@ -9,18 +9,41 @@ import Search from './Search';
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(BASE_URL)
       .then(response => response.json())
-      .then(json => setCharacters(json.results))
+      .then(jsonRes => {
+        setCharacters(jsonRes.results);
+        setFilteredCharacters(jsonRes.results);
+      })
+
       .catch(error => console.log(error))
       .finally(() => setLoading(false));
   }, []);
 
   const filterCards = function(e) {
-    console.log(e);
+    // Let's get the value the user typed in and make it lower case:
+    const searchValue = e.target.value.toLowerCase();
+
+    // create a new array from the characters array
+    const filteredArray = characters.filter(function(char) {
+      // make each name lowercase so we can check it properly with the search value
+      const lowerCaseName = char.name.toLowerCase();
+
+      // check if the character name begins with the search value
+      if (lowerCaseName.startsWith(searchValue)) {
+        // if it does, return true
+        // this will add it to the new filtered array
+        return true;
+      }
+      return false;
+    });
+
+    // set filtered characters to the new array
+    setFilteredCharacters(filteredArray);
   };
 
   if (loading) {
@@ -31,7 +54,7 @@ export default function CharacterList() {
     <>
       <Search handleSearch={filterCards} />
       <Row>
-        {characters.map(character => {
+        {filteredCharacters.map(character => {
           const {id, name, image} = character;
 
           return (
